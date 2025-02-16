d883788966894b2092a9b9e12dfb65a95ad90290
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-02-17 01:55:41 +0530

    file

--- packages/node/src/controllers/deploy.ts
+++ packages/node/src/controllers/deploy.ts
@@ -1,17 +1,22 @@
 import { Request, Response } from "express";
 import simpleGit from "simple-git";
 import { generate } from "../utils/generate";
+import { getAllFiles } from "../utils/getAllFiles";
+import path from 'path'
 
 export const deploy = async(req:Request,res:Response) => {
     try {
     const repoUrl = req.body.repoUrl;
     const id = generate();
     const git = simpleGit();
-    await git.clone(repoUrl, `output/${id}`);
+    await git.clone(repoUrl, path.join(__dirname,`output/${id}`));
     
+    const files = getAllFiles(path.join(__dirname,`output/${id}`));
+
     res.status(200).json({
         message:"Successful",
-        id
+        id,
+        files
     })
     return
     } catch(error) {
